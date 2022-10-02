a13d56d83581702257f2da4c233982678efe0f12
DineshShanmugamAnalyst <dinesh972719@gmail.com>
2022-10-02 22:47:37 +0530

    Singleton Demo

---
+++ src/org/cts/SingletonDemo.java
@@ -0,0 +1,35 @@
+package org.cts;
+
+public class SingletonDemo {
+
+	private SingletonDemo() {
+		
+	}
+		
+	public static SingletonDemo Object;	
+		
+		public  static SingletonDemo getObject() {  // we can create static variable for class;
+			
+			if (Object==null){
+			Object = new SingletonDemo();
+				
+			}
+			return Object;
+		}
+		
+	public static void main(String[] args) {
+		
+		SingletonDemo s1 =getObject(); 
+		
+		System.out.println(System.identityHashCode(s1));
+		
+	SingletonDemo s2 =getObject(); 
+		
+		System.out.println(System.identityHashCode(s2));
+		
+	SingletonDemo s3 = getObject ();
+		
+		System.out.println(System.identityHashCode(s3));
+	}	
+	}	
+
